4b6abb7db63f0e55e58fc45740b4242b9c99cd40
suraj <suraj@funnelstory.io>
2024-12-13 23:22:07 +0530

    chore: add page componentts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./sections/header.jsx";
+import Hero from "./sections/hero.jsx";
+import Features from "./sections/features.jsx";
+import Pricing from "./sections/pricing.jsx";
+import Faq from "./sections/faq.jsx";
+import Testimonials from "./sections/testimonials.jsx";
+import Download from "./sections/download.jsx";
+import Footer from "./sections/footer.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
+    </main>
+  );
+};
 
-export default App
+export default App;
